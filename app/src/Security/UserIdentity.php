60f7feb259d717cf62fa5cbd1efa5e34b9051068
maks-sl <maxislyshenkov@gmail.com>
2021-01-21 03:32:38 +0300

    Separate class UserIdentity for auth and its UserProvider

---
+++ app/src/Security/UserIdentity.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Model\User\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserIdentity implements UserInterface
+{
+    private $id;
+    private $username;
+    private $password;
+    private $display;
+    private $role;
+    private $status;
+
+    public function __construct(
+        string $id,
+        string $username,
+        string $password,
+        string $display,
+        string $role,
+        string $status
+    )
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->display = $display;
+        $this->role = $role;
+        $this->status = $status;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === User::STATUS_ACTIVE;
+    }
+
+    public function getDisplay(): string
+    {
+        return $this->display;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getRoles(): array
+    {
+        return [$this->role];
+    }
+
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    public function eraseCredentials(): void
+    {
+
+    }
+}
