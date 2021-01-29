d36c26d36172c5edffa6637d3d9fafb344430de3
maks-sl <maxislyshenkov@gmail.com>
2021-01-29 03:18:14 +0300

    Added simple functional HomeTest

---
+++ app/tests/Functional/HomeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testSuccess(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Welcome!', $crawler->filter('title')->text());
+    }
+}
